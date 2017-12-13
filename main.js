e301a333a96714700466aae027685c914239181f
PeerHenry <peerhenry@gmail.com>
2017-12-13 08:58:58 +0100

    circle square intersection test

--- main.js
+++ main.js
@@ -9,38 +9,96 @@
   setInterval(gameLoop, 1000/15);
 
   var speed = 5;
-  var x = 10;
-  var y = 10;
+  var x = 200;
+  var y = 200;
   var dx = 0;
   var dy = 0;
+  var fracRange = 50;
+  var maxRank = 2;
+
+  var pieces = [];
+  var recSize = 32;
+
+  // intersection
+  function intersect(rec, circle)
+  {
+    var points = [
+      {x: rec.x, y: rec.y},
+      {x: rec.x+rec.w, y: rec.y},
+      {x: rec.x, y: rec.y+rec.h},
+      {x: rec.x+rec.w, y: rec.y+rec.h},
+    ];
+    for(var n = 0; n<4; n++)
+    {
+      var nx = points[n].x;
+      var ny = points[n].y;
+      var dcx = nx - circle.x;
+      var dcy = ny - circle.y;
+      var dss = dcx*dcx + dcy*dcy;
+      if(dss < circle.r * circle.r) return true;
+    }
+    return false;
+  }
   
-  function drawGrid()
+  function updatePieces()
+  {
+    pieces = [];
+    var circle = {x: x, y: y, r: fracRange};
+    for(var i = 0; i < w/recSize; i++){
+      for(var j = 0; j < h/recSize; j++){
+        // intersection test with player circle
+        var px = recSize*i;
+        var py = recSize*j;
+        var rec = {x: px, y: py, w: recSize, h: recSize};
+        if( intersect(rec, circle) )
+        {
+          pieces.push({
+            x: px,
+            y: py,
+            w: recSize,
+            h: recSize,
+          });
+        }
+      }
+    }
+  }
+
+  function clear()
   {
     ctx.fillStyle = "black";
     ctx.fillRect(0,0,1024,1024);
+  }
 
+  function drawPieces()
+  {
     ctx.strokeStyle = "red";
-    var recSize = 32;
-    for(var i = 0; i < w/recSize; i++){
-      for(var j = 0; j < h/recSize; j++){
-        ctx.rect(
-          recSize*i,
-          recSize*j,
-          recSize,
-          recSize
-        );
-      }
+    //ctx.lineWidth = 1;
+    for(var n = 0; n < pieces.length; n++){
+      var p = pieces[n];
+      ctx.strokeRect(p.x, p.y, p.w, p.h);
     }
+  }
+
+  function drawPlayer()
+  {
+    x += dx*speed;
+    y += dy*speed;
+    ctx.fillStyle = "green";
+    ctx.strokeStyle = "green";
+    ctx.fillRect(x-4,y-4,8,8);
+
+    // draw high detail range
+    ctx.beginPath();
+    ctx.arc(x,y,fracRange,0,2*Math.PI);
     ctx.stroke();
   }
 
   function gameLoop()
   {
-    drawGrid();
-    x += dx*speed;
-    y += dy*speed;
-    ctx.fillStyle = "green";
-    ctx.fillRect(x,y,8,8);
+    updatePieces();
+    clear();
+    drawPieces();
+    drawPlayer();
   }
 
   function keyPush(ev)
